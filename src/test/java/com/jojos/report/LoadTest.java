a51789b1589d7b60310f8e633aaf9170585d62f2
Giorgos Karanikas <giorgos.karanikas@affinitas.de>
2017-03-25 10:10:22 +0100

    correct unit test

--- src/test/java/com/jojos/report/LoadTest.java
+++ src/test/java/com/jojos/report/LoadTest.java
@@ -1,5 +1,7 @@
 package com.jojos.report;
 
+import com.jojos.report.data.Department;
+import com.jojos.report.job.Loader;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -12,12 +14,13 @@ public class LoadTest {
 
     @Test
     public void loadDepartments() {
-        Department department = new Department("Accounting");
-        Department department = new Department("Human Resources");
+        Department department1 = new Department("Accounting");
+        Department department2 = new Department("Human Resources");
 
         Loader loader = new Loader();
 
-        loader.load(department);
+        loader.load(department1);
+        loader.load(department2);
 
         Assert.assertTrue("departments count expected to be 2", loader.departmentsSize() == 2);
     }
