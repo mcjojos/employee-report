e05ce8f47fe69b2b63948237455dc880a1752ce6
Giorgos Karanikas <giorgos.karanikas@affinitas.de>
2017-03-25 10:10:54 +0100

    added department object

---
+++ src/main/java/com/jojos/report/data/Department.java
@@ -0,0 +1,48 @@
+package com.jojos.report.data;
+
+/**
+ * Data object which is as its name suggests representing a department
+ * @author karanikasg@gmail.com
+ */
+public class Department implements Comparable<Department> {
+
+    private final String name;
+
+    public Department(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Department that = (Department) o;
+
+        return name != null ? name.equals(that.name) : that.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Department{" +
+                name + '}';
+    }
+
+    @Override
+    public int compareTo(Department that) {
+        if (this.name == that.name) {
+            return 0;
+        }
+
+        if (this.name == null) {
+            return -1;
+        }
+
+        return this.name.compareTo(that.name);
+    }
+}
---
+++ src/main/java/com/jojos/report/job/Loader.java
@@ -0,0 +1,35 @@
+package com.jojos.report.job;
+
+import com.jojos.report.data.Department;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.TreeMap;
+
+/**
+ * The purpose of the loader class is to load departments, employees and it's attributes
+ *
+ * The internal representation remains hidden from the outside world by providing only the API
+ * necessary to expose retrieving elements and keeping them in order.
+ *
+ * TODO:
+ * More specifically the departments are kept sorted by alphabetic order.
+ *
+ * @author karanikasg@gmail.com
+ */
+public class Loader {
+
+    private final List<Department> departments = new ArrayList<>();
+
+    public void load(Department department) {
+        int pos = Collections.binarySearch(departments, department);
+        if (pos < 0) {
+            departments.add(-pos-1, department);
+        }
+    }
+
+    public int departmentsSize() {
+        return departments.size();
+    }
+}
